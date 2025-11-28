0e881a905bab7e6b1e9d79fa829eee1a538fb434
Mohamamd Alobiad <moh.alobiad@gmail.com>
2025-11-28 14:55:13 +0300

    Add Arabic term name support for attribute values

--- mystaging01/wp-content/plugins/alsaadrose-attribute-translations/alsaadrose-attribute-translations.php
+++ mystaging01/wp-content/plugins/alsaadrose-attribute-translations/alsaadrose-attribute-translations.php
@@ -10,7 +10,8 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-const ASATTR_OPTION_KEY = 'asattr_attribute_names_ar';
+const ASATTR_OPTION_KEY      = 'asattr_attribute_names_ar';
+const ASATTR_TERM_OPTION_KEY = 'asattr_attribute_term_names_ar';
 
 /**
  * Get the saved Arabic attribute names.
@@ -19,6 +20,21 @@ const ASATTR_OPTION_KEY = 'asattr_attribute_names_ar';
  */
 function asattr_get_arabic_attribute_names() {
     $stored = get_option( ASATTR_OPTION_KEY, array() );
+
+    if ( ! is_array( $stored ) ) {
+        return array();
+    }
+
+    return $stored;
+}
+
+/**
+ * Get the saved Arabic attribute term names.
+ *
+ * @return array<string, array<string, string>>
+ */
+function asattr_get_arabic_attribute_term_names() {
+    $stored = get_option( ASATTR_TERM_OPTION_KEY, array() );
 
     if ( ! is_array( $stored ) ) {
         return array();
@@ -58,6 +74,49 @@ function asattr_save_attribute_name( $slug, $value ) {
  */
 function asattr_remove_attribute_name( $slug ) {
     asattr_save_attribute_name( $slug, '' );
+}
+
+/**
+ * Save or remove an Arabic term name for a given taxonomy/slug pair.
+ *
+ * @param string $taxonomy Taxonomy name.
+ * @param string $slug     Term slug.
+ * @param string $value    Arabic term name.
+ */
+function asattr_save_attribute_term_name( $taxonomy, $slug, $value ) {
+    $taxonomy = sanitize_key( $taxonomy );
+    $slug     = sanitize_title( $slug );
+
+    if ( '' === $taxonomy || '' === $slug ) {
+        return;
+    }
+
+    $names = asattr_get_arabic_attribute_term_names();
+
+    if ( ! isset( $names[ $taxonomy ] ) || ! is_array( $names[ $taxonomy ] ) ) {
+        $names[ $taxonomy ] = array();
+    }
+
+    if ( '' === $value ) {
+        unset( $names[ $taxonomy ][ $slug ] );
+        if ( empty( $names[ $taxonomy ] ) ) {
+            unset( $names[ $taxonomy ] );
+        }
+    } else {
+        $names[ $taxonomy ][ $slug ] = $value;
+    }
+
+    update_option( ASATTR_TERM_OPTION_KEY, $names );
+}
+
+/**
+ * Remove stored Arabic term name.
+ *
+ * @param string $taxonomy Taxonomy name.
+ * @param string $slug     Term slug.
+ */
+function asattr_remove_attribute_term_name( $taxonomy, $slug ) {
+    asattr_save_attribute_term_name( $taxonomy, $slug, '' );
 }
 
 /**
@@ -122,6 +181,78 @@ function asattr_render_edit_field() {
 add_action( 'woocommerce_after_edit_attribute_fields', 'asattr_render_edit_field' );
 
 /**
+ * Check if a taxonomy is an attribute taxonomy.
+ *
+ * @param string $taxonomy Taxonomy name.
+ *
+ * @return bool
+ */
+function asattr_is_attribute_taxonomy( $taxonomy ) {
+    return 0 === strpos( $taxonomy, 'pa_' );
+}
+
+/**
+ * Render Arabic name input on add term form for attribute taxonomies.
+ *
+ * @param string $taxonomy Taxonomy name.
+ */
+function asattr_render_term_add_field( $taxonomy ) {
+    if ( ! asattr_is_attribute_taxonomy( $taxonomy ) ) {
+        return;
+    }
+    ?>
+    <div class="form-field">
+        <label for="name_AR"><?php esc_html_e( 'Arabic name', 'asattr' ); ?></label>
+        <input name="name_AR" id="name_AR" type="text" value="" />
+        <p class="description"><?php esc_html_e( 'Optional Arabic label used when viewing the store in Arabic.', 'asattr' ); ?></p>
+    </div>
+    <?php
+}
+
+/**
+ * Render Arabic name input on edit term form for attribute taxonomies.
+ *
+ * @param WP_Term $term     Current term.
+ * @param string  $taxonomy Taxonomy name.
+ */
+function asattr_render_term_edit_field( $term, $taxonomy ) {
+    if ( ! asattr_is_attribute_taxonomy( $taxonomy ) ) {
+        return;
+    }
+
+    $saved = asattr_get_arabic_attribute_term_names();
+    $value = '';
+
+    if ( isset( $saved[ $taxonomy ][ $term->slug ] ) ) {
+        $value = $saved[ $taxonomy ][ $term->slug ];
+    }
+    ?>
+    <tr class="form-field">
+        <th scope="row" valign="top"><label for="name_AR"><?php esc_html_e( 'Arabic name', 'asattr' ); ?></label></th>
+        <td>
+            <input name="name_AR" id="name_AR" type="text" value="<?php echo esc_attr( $value ); ?>" />
+            <p class="description"><?php esc_html_e( 'Optional Arabic label used when viewing the store in Arabic.', 'asattr' ); ?></p>
+        </td>
+    </tr>
+    <?php
+}
+
+/**
+ * Register add/edit term field hooks for all attribute taxonomies.
+ */
+function asattr_register_term_field_hooks() {
+    if ( ! function_exists( 'wc_get_attribute_taxonomy_names' ) ) {
+        return;
+    }
+
+    foreach ( wc_get_attribute_taxonomy_names() as $taxonomy ) {
+        add_action( "{$taxonomy}_add_form_fields", 'asattr_render_term_add_field' );
+        add_action( "{$taxonomy}_edit_form_fields", 'asattr_render_term_edit_field', 10, 2 );
+    }
+}
+add_action( 'init', 'asattr_register_term_field_hooks', 20 );
+
+/**
  * Persist the Arabic name when a new attribute is created.
  *
  * @param int   $id   Attribute ID.
@@ -167,6 +298,29 @@ function asattr_handle_attribute_updated( $id, $data, $old_slug ) {
 add_action( 'woocommerce_attribute_updated', 'asattr_handle_attribute_updated', 10, 3 );
 
 /**
+ * Persist Arabic term name on create/edit actions for attribute taxonomies.
+ *
+ * @param int    $term_id Term ID.
+ * @param int    $tt_id   Term taxonomy ID.
+ * @param string $taxonomy Taxonomy name.
+ */
+function asattr_handle_term_saved( $term_id, $tt_id, $taxonomy ) {
+    if ( ! asattr_is_attribute_taxonomy( $taxonomy ) ) {
+        return;
+    }
+
+    $arabic_name = isset( $_POST['name_AR'] ) ? sanitize_text_field( wp_unslash( $_POST['name_AR'] ) ) : ''; // phpcs:ignore WordPress.Security.NonceVerification.Missing
+
+    $term = get_term( $term_id, $taxonomy );
+
+    if ( $term instanceof WP_Term ) {
+        asattr_save_attribute_term_name( $taxonomy, $term->slug, $arabic_name );
+    }
+}
+add_action( 'created_term', 'asattr_handle_term_saved', 10, 3 );
+add_action( 'edited_term', 'asattr_handle_term_saved', 10, 3 );
+
+/**
  * Clean up saved Arabic names when an attribute is deleted.
  *
  * @param int    $id       Attribute ID.
@@ -176,8 +330,37 @@ add_action( 'woocommerce_attribute_updated', 'asattr_handle_attribute_updated', 
 function asattr_handle_attribute_deleted( $id, $name, $taxonomy ) {
     $slug = $taxonomy ? wc_attribute_taxonomy_slug( $taxonomy ) : wc_sanitize_taxonomy_name( $name );
     asattr_remove_attribute_name( $slug );
+
+    $terms = asattr_get_arabic_attribute_term_names();
+
+    if ( isset( $terms[ $taxonomy ] ) ) {
+        unset( $terms[ $taxonomy ] );
+        update_option( ASATTR_TERM_OPTION_KEY, $terms );
+    }
 }
 add_action( 'woocommerce_attribute_deleted', 'asattr_handle_attribute_deleted', 10, 3 );
+
+/**
+ * Remove stored Arabic term name when an attribute term is deleted.
+ *
+ * @param int         $term       Term ID.
+ * @param int         $tt_id      Term taxonomy ID.
+ * @param string      $taxonomy   Taxonomy name.
+ * @param WP_Term|int $deleted    Deleted term object or ID depending on WP version.
+ * @param array       $object_ids Object IDs affected.
+ */
+function asattr_handle_term_deleted( $term, $tt_id, $taxonomy, $deleted, $object_ids ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+    if ( ! asattr_is_attribute_taxonomy( $taxonomy ) ) {
+        return;
+    }
+
+    $term_obj = $deleted instanceof WP_Term ? $deleted : get_term( $term, $taxonomy );
+
+    if ( $term_obj instanceof WP_Term ) {
+        asattr_remove_attribute_term_name( $taxonomy, $term_obj->slug );
+    }
+}
+add_action( 'delete_term', 'asattr_handle_term_deleted', 10, 5 );
 
 /**
  * Detect the current TranslatePress language code.
@@ -194,6 +377,24 @@ function asattr_get_trp_language_code() {
 
     if ( isset( $GLOBALS['TRP_LANGUAGE'] ) && is_string( $GLOBALS['TRP_LANGUAGE'] ) ) {
         return $GLOBALS['TRP_LANGUAGE'];
+    }
+
+    return '';
+}
+
+/**
+ * Lookup stored Arabic term name for taxonomy/slug pair.
+ *
+ * @param string $taxonomy Taxonomy name.
+ * @param string $slug     Term slug.
+ *
+ * @return string
+ */
+function asattr_get_arabic_term_name( $taxonomy, $slug ) {
+    $map = asattr_get_arabic_attribute_term_names();
+
+    if ( isset( $map[ $taxonomy ][ $slug ] ) ) {
+        return $map[ $taxonomy ][ $slug ];
     }
 
     return '';
@@ -248,3 +449,95 @@ function asattr_filter_attribute_label( $label, $name, $product ) {
     return $label;
 }
 add_filter( 'woocommerce_attribute_label', 'asattr_filter_attribute_label', 10, 3 );
+
+/**
+ * Replace the name property on a term object with its Arabic equivalent if available.
+ *
+ * @param WP_Term $term Term object.
+ *
+ * @return WP_Term
+ */
+function asattr_maybe_apply_term_translation( $term ) {
+    if ( ! ( $term instanceof WP_Term ) ) {
+        return $term;
+    }
+
+    if ( ! asattr_is_attribute_taxonomy( $term->taxonomy ) ) {
+        return $term;
+    }
+
+    if ( ! asattr_is_arabic_language() ) {
+        return $term;
+    }
+
+    $arabic = asattr_get_arabic_term_name( $term->taxonomy, $term->slug );
+
+    if ( '' !== $arabic ) {
+        $term->name = $arabic;
+    }
+
+    return $term;
+}
+
+/**
+ * Filter single term retrieval for attribute taxonomies.
+ *
+ * @param WP_Term|WP_Error $term     Term object or error.
+ * @param string           $taxonomy Taxonomy name.
+ *
+ * @return WP_Term|WP_Error
+ */
+function asattr_filter_get_term( $term, $taxonomy ) {
+    if ( is_wp_error( $term ) ) {
+        return $term;
+    }
+
+    return asattr_maybe_apply_term_translation( $term );
+}
+add_filter( 'get_term', 'asattr_filter_get_term', 10, 2 );
+
+/**
+ * Filter multiple term retrieval for attribute taxonomies.
+ *
+ * @param array       $terms      Array of terms or WP_Error.
+ * @param string[]    $taxonomies Requested taxonomies.
+ *
+ * @return array|WP_Error
+ */
+function asattr_filter_get_terms( $terms, $taxonomies ) {
+    if ( is_wp_error( $terms ) ) {
+        return $terms;
+    }
+
+    if ( empty( $terms ) || ! is_array( $terms ) ) {
+        return $terms;
+    }
+
+    foreach ( $terms as $index => $term ) {
+        $terms[ $index ] = asattr_maybe_apply_term_translation( $term );
+    }
+
+    return $terms;
+}
+add_filter( 'get_terms', 'asattr_filter_get_terms', 10, 2 );
+
+/**
+ * Swap variation option labels to Arabic when available.
+ *
+ * @param string          $term_name Display term name.
+ * @param WP_Term         $term      Term object.
+ * @param string          $attribute Attribute name.
+ * @param WC_Product|null $product   Product object.
+ *
+ * @return string
+ */
+function asattr_filter_variation_option_name( $term_name, $term, $attribute, $product ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+    if ( ! ( $term instanceof WP_Term ) ) {
+        return $term_name;
+    }
+
+    $translated = asattr_maybe_apply_term_translation( $term );
+
+    return $translated instanceof WP_Term ? $translated->name : $term_name;
+}
+add_filter( 'woocommerce_variation_option_name', 'asattr_filter_variation_option_name', 10, 4 );
